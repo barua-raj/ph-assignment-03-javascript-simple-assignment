702569737e8a72799f892ccd60ae9840552e6d95
barua-raj <barua.jovraj@gmail.com>
2025-08-04 15:25:19 +0600

    problem 06 current salary solved

---
+++ problem-06-current-salary.js
@@ -0,0 +1,13 @@
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+
+var currentSalary = startingSalary * (1 + (5 / 100))**experience
+
+console.log(currentSalary.toFixed(2));
+
+
+
+
+// C = P * (1 + r)**n
